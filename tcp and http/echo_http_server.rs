22092df236946d13d7071c2ecce9c6d72d06cb9a
gaoxinge <gaoxx5@gmail.com>
2021-10-22 00:37:21 +0800

    tcp and http

---
+++ tcp and http/echo_http_server.rs
@@ -0,0 +1,19 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    stream.write(&mut buffer).unwrap();
+    stream.flush().unwrap();
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
